e2b0630b3667d20dc10e66fb89968fd8c23c1b90
h.js <121519834+hyeon-js@users.noreply.github.com>
2023-12-06 23:54:46 +0900

    Update MainActivity.java

--- java/com/hyeonjs/gfriendmusicnumber/MainActivity.java
+++ java/com/hyeonjs/gfriendmusicnumber/MainActivity.java
@@ -2,11 +2,16 @@ package com.hyeonjs.gfriendmusicnumber;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+
+        setContentView(layout);
     }
 }
