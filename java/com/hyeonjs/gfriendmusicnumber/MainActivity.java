869a78e909ab30b7dfa11cf514411d4c168eeaad
h.js <121519834+hyeon-js@users.noreply.github.com>
2023-12-20 05:05:49 +0900

    Update MainActivity.java

--- java/com/hyeonjs/gfriendmusicnumber/MainActivity.java
+++ java/com/hyeonjs/gfriendmusicnumber/MainActivity.java
@@ -2,9 +2,11 @@ package com.hyeonjs.gfriendmusicnumber;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -18,16 +20,31 @@ public class MainActivity extends Activity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(1);
 
-        String[] names = {"test1", "test2"};
+        Pair<String[], String[]> data = createSongList();
         ListView list = new ListView(this);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data.first);
         list.setAdapter(adapter);
         layout.addView(list);
 
         setContentView(layout);
     }
 
-
+    private Pair<String[], String[]> createSongList() {
+        try {
+            String[] data = readData(getAssets().open("songs.csv")).split("\n");
+            String[] names = new String[data.length];
+            String[] list = new String[data.length];
+            for (int n = 0; n < data.length; n++) {
+                String[] datum = data[n].split(",");
+                names[n] = datum[0];
+                list[n] = data[n];
+            }
+            return new Pair<>(names, list);
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            return new Pair<>(new String[]{}, new String[]{});
+        }
+    }
 
     private String readData(InputStream stream) {
         try {
